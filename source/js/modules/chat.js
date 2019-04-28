0cab1b5c65f0e7518486ae9ba94db056ea1b2013
Alex Nesvit <nesvit.alexander@gmail.com>
2019-04-28 18:49:00 +0300

    fixed fetch

--- source/js/modules/chat.js
+++ source/js/modules/chat.js
@@ -85,7 +85,7 @@ export default class {
   }
 
   getUsers() {
-    return fetch("/dist/users.json")
+    return fetch("/test-task/dist/users.json")
       .then(response => response.json())
       .then(data => data);
   }
